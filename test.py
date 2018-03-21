a993ab9210d0d4bfab81c02c8e540794d4cd5c5f
w1ncheste2-pc\Rawin <winchester.rawin@gmail.com>
2018-03-21 21:41:01 +0700

    create training data

--- test.py
+++ test.py
@@ -8,22 +8,38 @@ import os
 
 def keys_to_output(keys):
 	#[Z,X,C,left,up,right,down]
-	output = [0,0,0]
-	if 'Z' in keys:
-		output[0] = 1
-	elif 'X' in keys:
-		output[1] = 1
-	elif 'C' in keys:
-		output[2] = 1
-	elif 'left' in keys:
-		output[3] = 1
-	elif 'up' in keys:
-		output[4] = 1
-	elif 'right' in keys:
-		output[5] = 1
-	elif 'down' in keys:
-		output[6] = 1
-	return output		
+    output = [0,0,0,0,0,0,0]
+    if 'Z' in keys:
+        output[0] = 1
+        print('z')
+    elif 'X' in keys:
+        output[1] = 1
+        print('x')
+    elif 'C' in keys:
+        output[2] = 1
+        print('c')
+    elif 'left' in keys:
+        output[3] = 1
+        print('left')
+    elif 'up' in keys:
+        output[4] = 1
+        print('up')
+    elif 'right' in keys:
+        output[5] = 1
+        print('right')
+    elif 'down' in keys:
+        output[6] = 1
+        print('down')
+    return output		
+file_name ='trainingdata.npy'
+if os.path.isfile(file_name):
+    print ('loading')
+    training_data = list(np.load(file_name))
+else:
+    print('creating file')
+    training_data = []
+
+
 
 def process_img(original_image):
     
@@ -31,6 +47,9 @@ def process_img(original_image):
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     # edge detection
     processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    keys = key_check()
+    output = keys_to_output(keys)
+    training_data.append([processed_img,output])
     return processed_img
 
 for i in list(range(4))[::-1]:
@@ -43,7 +62,8 @@ while(True):
     # 800x600
     # 40 px accounts for title bar. 
     screen = np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
-    new_screen = process_img(screen)
+    new_screen = process_img(screen)#display processed img
+
 
     #print('jump')
     #PressKey(Z)
@@ -51,9 +71,13 @@ while(True):
     #print('fire')
     #PressKey(X)
 
-    print('loop took {} seconds'.format(time.time()-last_time))
+    #print('loop took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
+
     cv2.imshow('window', new_screen)
+    if len(training_data) % 500 == 0 :
+        np.save(file_name,training_data)
+        print('saved')
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
