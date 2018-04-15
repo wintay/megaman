792ec65fa3753e8774f83b098c5aa6872f88dedd
newpalita <newpalita@gmail.com>
2018-04-16 03:23:56 +0700

    autosave training data

--- test.py
+++ test.py
@@ -148,13 +148,13 @@ while(True):
     last_time = time.time()
 
     cv2.imshow('window', new_screen)
-    # if len(training_data) % 50 == 0 :
-    #     np.save(file_name,training_data)
-    #     print('saved')
+    if len(training_data) % 500 == 0 :
+        np.save(file_name,training_data)
+        print('saved')
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
 
-screen_record()
+# screen_record()
 
 #VID9 10.21
